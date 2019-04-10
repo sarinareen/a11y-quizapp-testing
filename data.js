34fffc33d8f29e7df8c0b152c09c4481eea08880
Sarina Carr <sarina.b.carr@gmail.com>
2019-04-10 12:01:50 -0400

    Added files

---
+++ data.js
@@ -0,0 +1,112 @@
+ const store = [
+    {
+      question: 'Which of these musicians is a famous bass player?',
+      answers: [
+        'Jimi Hendrix', // 0
+        'James Jamerson', // 1
+        'Lana Del Ray', // 2
+        'Janis Joplin', // 3
+      ],
+      correctAnswer: 'James Jamerson'
+    },
+
+    {
+      question: 'Rhythm is an important part of mastering bass - what is it?',
+      answers: [
+        'A group of three notes sounded together as a basis of harmony',
+        'A regular or repeated pattern of sound',
+        'The distance in pitch between two notes',
+        'The speed at which a passage of music is played',
+      ],
+      correctAnswer: 'A regular or repeated pattern of sound',
+    },
+
+    {
+      question: 'What is a popular brand of electric bass guitars?',
+      answers: [
+        'Ford',
+        'Rickenbacker',
+        'Masher',
+        'Cryptonia',
+      ],
+      correctAnswer: 'Rickenbacker',
+    },
+
+    {
+      question: 'What is a fret on the bass guitar?',
+      answers: [
+        'A transducer that captures mechanical vibrations',
+        'A thin, metal strip embedded on along the length of the fingerboard',
+        'A small piece of wood that provides a groove for each string',
+        'A knob used for adjusting volume and tone',
+      ],
+      correctAnswer: 'A thin, metal strip embedded on along the length of the fingerboard',
+    },
+
+    {
+      question: 'Who is credited as inventing the electric bass guitar?',
+      answers: [
+        'Leo Fender',
+        'Jaco Pastorius',
+        'Les Claypool',
+        'Paul Tutmarc',
+      ],
+      correctAnswer: 'Paul Tutmarc',
+    },
+
+    {
+      question: 'How many strings does an electric bass guitar most commonly have?',
+      answers: [
+        '4',
+        '5',
+        '7',
+        '6',
+      ],
+      correctAnswer: '4',
+    },
+
+    {
+      question: 'What part of the bass attaches the strings to the end of the body of a bass?',
+      answers: [
+        'truss rod',
+        'pickups',
+        'nut',
+        'bridge',
+      ],
+      correctAnswer:'bridge',
+    },
+
+    {
+      question: 'Name on technique of plucking the strings.',
+      answers: [
+        'Fingerboard',
+        'Alternate',
+        'Back-and-forth',
+        'Slow motion',
+      ],
+      correctAnswer:'Alternate',
+    },
+
+    {
+      question: 'What does "slapping the bass" mean?',
+      answers: [
+        'Throwing your bass guitar on the ground',
+        'Using a pick on the strings',
+        'Making a percussive sound by bouncing the strings',
+        'Breaking a bass amp',
+      ],
+      correctAnswer: 'Making a percussive sound by bouncing the strings',
+    },
+
+    {
+      question: 'Who is credited as the first to play by "slapping" the bass, and made this technique popular?',
+      answers: [
+        'Flea',
+        'Larry Graham',
+        'Sly Stone',
+        'Marvin Gaye',
+      ],
+      correctAnswer: 'Larry Graham',
+    }
+];
+
